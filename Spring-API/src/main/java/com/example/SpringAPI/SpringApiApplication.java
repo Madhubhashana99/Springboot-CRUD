a1ffe8cb1d02f5f7c2be7d4738ab39670129f423
Madhubhashana99 <104136158+Madhubhashana99@users.noreply.github.com>
2023-03-10 00:37:13 +0530

    just update

--- Spring-API/src/main/java/com/example/SpringAPI/SpringApiApplication.java
+++ Spring-API/src/main/java/com/example/SpringAPI/SpringApiApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringApiApplication.class, args);
-	}
+	public static void main(String[] args) {SpringApplication.run(SpringApiApplication.class, args);}
 
 }
