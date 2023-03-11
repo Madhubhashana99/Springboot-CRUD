d98c63157d93a6a42e1bab2c465f22e952832a41
Madhubhashana99 <104136158+Madhubhashana99@users.noreply.github.com>
2023-03-11 21:34:10 +0530

    updated

--- Spring-API/src/main/java/com/example/SpringAPI/controller/EmployeeController.java
+++ Spring-API/src/main/java/com/example/SpringAPI/controller/EmployeeController.java
@@ -1,8 +1,10 @@
 package com.example.SpringAPI.controller;
 
+import com.example.SpringAPI.exception.ResourseNotFoundException;
 import com.example.SpringAPI.model.Employee;
 import com.example.SpringAPI.repo.EmployeeRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,6 +24,12 @@ public class EmployeeController {
     @PostMapping
     public Employee createEmployee(@RequestBody Employee employee){
         return employeeRepo.save(employee);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable long id){
+        Employee employee = employeeRepo.findById(id).orElseThrow(()->new ResourseNotFoundException("Employee exist with id:" + id));
+        return  ResponseEntity.ok(employee);
     }
 
 
